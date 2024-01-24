c4c348f76647f23e7c6be8dd478f7fd72576ddc0
VulumeCode <2590984+VulumeCode@users.noreply.github.com>
2024-01-24 23:04:11 +0100

    grid

--- beancounter/src/App.tsx
+++ beancounter/src/App.tsx
@@ -26,13 +26,11 @@ function App() {
   });
   
   const [points, set_points] = useState<number>(0);
-  const [mult, set_mult] = useState<number>(1);
-  const [gos, set_gos] = useState<number>(0);
-  const score = calcScore(points, mult, gos);
+  const [dups, set_dups] = useState<number>(0);
+  const score = calcScore(points, dups);
   const initScore = () => {
     set_points(0);
-    set_mult(1);
-    set_gos(0);
+    set_dups(0);
   }
 
 
@@ -56,29 +54,24 @@ function App() {
         <div>---</div>
         <div id="pointinput">
           <button id="rst" onClick={initScore}>↺</button>
-          <button id="pp" onClick={()=>set_points(points+1)}>▲</button><div id="vp">{points}</div><button id="mp" onClick={()=>set_points(points-1)}>▼</button>
-          <button id="pm" onClick={()=>set_mult(mult+1)}>▲</button><div id="vm">×{mult}</div><button id="mm" onClick={()=>set_mult(mult-1)}>▼</button>
-          <button id="pg" onClick={()=>set_gos(gos+1)}>▲</button><div id="vg">{gos} 고</div><button id="mg" onClick={()=>set_gos(gos-1)}>▼</button>
-          <div id="vs"> = ₩{score}</div>
+          
+          <button id="pp" onClick={() => set_points(points + 1)}>▲</button>
+          <div id="vp">{points} <span className="coin">Pt</span></div>
+          <button id="mp" onClick={() => set_points(points - 1)}>▼</button>
+          
+          <button id="pm" onClick={() => set_dups(dups + 1)}>▲</button>
+          <div id="vm">{dups} <span className="coin">×2</span></div>
+          <button id="mm" onClick={() => set_dups(dups - 1)}>▼</button>
+          
+          <div id="vs"> = {score} <span className="coin">Pt</span></div>
         </div>
       </header>
     </div>
   );
 }
 
-const calcScore = (points: number, mult: number, gos: number): number => {
-  switch (gos) {
-    case 0:
-      break;
-    case 1:
-      points += 1;
-      break;
-    default:
-      points += 2;
-      mult += (gos - 2);
-      break;
-  }
-  return points * mult;
+const calcScore = (points: number, dups: number): number => {
+  return points * (2**dups);
 }
 
 export default App;
