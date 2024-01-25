a700a825a0b3ac0467d4c89fb400bf8367cac367
VulumeCode <2590984+VulumeCode@users.noreply.github.com>
2024-01-25 02:51:18 +0100

    pay

--- beancounter/src/App.tsx
+++ beancounter/src/App.tsx
@@ -8,70 +8,123 @@ import { useImmer } from "use-immer";
 type Player = {
   name: string;
   score: number;
+  liable: boolean;
+  dups: number;
 }
 
 type Game = {
   players: Player[];
 }
 
-
+const makePlayer = (name: string): Player => {
+  return {
+    name,
+    score: 88,
+    liable: false,
+    dups: 0,
+  }
+}
 
 function App() {
   const [game, set_game] = useImmer<Game>({
     players: [
-      { name: "Vincent", score: 88 },
-      { name: "Emily", score: 88 },
-      { name: "Jules", score: 88 },
+      makePlayer("Vincent"),
+      makePlayer("Emily"),
+      makePlayer("Jules"),
     ]
   });
-  
+
   const [points, set_points] = useState<number>(0);
   const [dups, set_dups] = useState<number>(0);
-  const score = calcScore(points, dups);
   const initScore = () => {
     set_points(0);
     set_dups(0);
   }
 
+  const score = calcScore(points, dups);
+  const liables = game.players.filter(p => p.liable);
+  const someoneIsLiable = liables.length > 0;
 
   return (
     <div className="App">
       <header className="App-header">
         {
-          game.players.map((player, i) => 
+          game.players.map((player, i) =>
             <div
               key={i}
-              >{player.name}: ₩{player.score}</div>
-            )
+            >{player.name}: {player.score} <Coin>Pt</Coin>
+
+              <button
+                className={"liable" + (player.liable ? " enabled" : " ")}
+                onClick={() => set_game((draft) => { draft.players[i].liable = !player.liable })}>Lose
+              </button>
+              {
+                player.liable &&
+                <>
+                  <button>
+                    <Coin>×2</Coin>
+                  </button>
+                  <button>
+                    <Coin>×2</Coin>
+                  </button>
+                </>
+              }
+              {
+                !!someoneIsLiable &&
+                !player.liable &&
+                <button
+                  className={"pay"}
+                  onClick={() => set_game((draft) => {
+                    draft.players.forEach(p => {
+                          if (p.liable) {
+                            p.score -= score;
+                            p.liable = false;
+                            p.dups = 0;
+                          }
+                        }
+                      )
+                    draft.players[i].score += (liables.length * score);
+                  })}
+                  >
+                  Pay
+                </button>
+              }
+            </div>
+          )
         }
+        <div>---</div>
         <div
-              onClick={() =>{
-                const name = window.prompt("Enter name")
-                if (!!name) {
-                  set_game((draft)=>{draft.players.push({name:name,score:88})})
-                }
+          onClick={() => {
+            const name = window.prompt("Enter name")
+            if (!!name) {
+              set_game((draft) => { draft.players.push(makePlayer(name)) })
+            }
           }}>Add player+</div>
         <div>---</div>
         <div id="pointinput">
           <button id="rst" onClick={initScore}>↺</button>
-          
+
           <button id="pp" onClick={() => set_points(points + 1)}>▲</button>
-          <div id="vp">{points} <span className="coin">Pt</span></div>
-          <button id="mp" onClick={() => set_points(points - 1)}>▼</button>
-          
+          <div id="vp">{points} <Coin>Pt</Coin></div>
+          <button id="mp" onClick={() => set_points(points - 1)} disabled={points < 1}>▼</button>
+
           <button id="pm" onClick={() => set_dups(dups + 1)}>▲</button>
-          <div id="vm">{dups} <span className="coin">×2</span></div>
-          <button id="mm" onClick={() => set_dups(dups - 1)}>▼</button>
-          
-          <div id="vs"> = {score} <span className="coin">Pt</span></div>
+          <div id="vm">{dups} <Coin>×2</Coin></div>
+          <button id="mm" onClick={() => set_dups(dups - 1)} disabled={dups < 1}>▼</button>
+
+          <div id="vs"> = {score} <Coin>Pt</Coin></div>
         </div>
       </header>
     </div>
   );
 }
 
+function Coin(props: { children: string }) {
+  return (<span className="coin">{props.children}</span>);
+}
+
 const calcScore = (points: number, dups: number): number => {
-  return points * (2**dups);
+  return points * (2 ** dups);
 }
 
 export default App;
